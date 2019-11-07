61627c1af394fa113807a0fe6728c976d107dd6e
Tom Hinton <tomjhinton@gmail.com>
2019-11-07 23:36:41 +0000

    upgrades

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,18 @@ import React from 'react'
 export const resourcesArr = []
 
 
+
+
+
 class Main extends React.Component {
 
 
   constructor(props) {
     super(props)
     this.state = {
-      modalState: true
+      modalState: true,
+      total: 0,
+      upgrades: false
 
     }
 
@@ -18,15 +23,17 @@ class Main extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.newButton = this.newButton.bind(this)
+    this.upgrade = this.upgrade.bind(this)
+    this.unlockUpgrades = this.unlockUpgrades.bind(this)
 
 
   }
 
 
   update(){
-    let  total = resourcesArr.map(x => x = x.collected)
-    console.log(total.reduce((a,b)=> a+b,0))
-    resourcesArr.map(x=> x.collected += ((1/x.id)*x.amount))
+    let total = resourcesArr.map(x => x = x.collected)
+    total = total.reduce((a,b)=> a+b,0)
+    resourcesArr.map(x=> x.collected += ((1/x.id)*x.amount)*x.upgradeLevel)
     resourcesArr.map(x=> {
       if(x.collected > x.id*100){
         x.canCreate = true
@@ -36,19 +43,26 @@ class Main extends React.Component {
       }
     })
 
-
+    this.setState({...this.state, total: {total} })
   }
 
 
   componentDidMount() {
 
     this.interval = setInterval(() => {
-      this.setState({ time: 1 })
+      this.setState({ ...this.state,time: 1 })
       this.update()
     }, 100)
 
   }
 
+  formatCash(n) {
+    if (n < 1e3) return n
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K'
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M'
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B'
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
+  }
 
 
   resourceCreator(name, building) {
@@ -66,18 +80,28 @@ class Main extends React.Component {
   }
 
   buyBuilding(e){
+    const  select = resourcesArr[e.target.id-1]
+    if(select.collected > select.id*(select.amount*10)) {
+      select.collected -= select.id*(select.amount*10)
+      select.amount++
 
-    if(resourcesArr[e.target.id-1].collected > resourcesArr[e.target.id-1].id*(resourcesArr[e.target.id-1].amount*10)) {
-      resourcesArr[e.target.id-1].collected -= resourcesArr[e.target.id-1].id*(resourcesArr[e.target.id-1].amount*10)
-      resourcesArr[e.target.id-1].amount++
+    }
+
+  }
+
+  upgrade(e){
+    const  select = resourcesArr[e.target.id-1]
+    if(select.collected > select.id*(select.amount*100)*(select.upgradeLevel)) {
+      select.collected -= select.id*(select.amount*100*(select.upgradeLevel))
+      select.upgradeLevel +=0.1
 
     }
 
   }
 
 
-
   handleSubmit(event) {
+
     new this.resourceCreator(this.state.resource, this.state.createdBy)
     this.toggleModal()
     event.preventDefault()
@@ -91,13 +115,19 @@ class Main extends React.Component {
 
 
 
-
+  unlockUpgrades(){
+    console.log(this.state.total)
+    if(this.state.total.total > 50000){
+      this.setState({...this.state, upgrades: true})
+      resourcesArr.map(x=> x.collected = 0)
+    }
+  }
 
 
 
   toggleModal(){
 
-    this.setState((prev, props) => {
+    this.setState((prev) => {
       const newState = !prev.modalState
 
       return { modalState: newState }
@@ -115,6 +145,8 @@ class Main extends React.Component {
     return(
 
       <div className="container">
+        {this.state.total && <h1>Total: {this.formatCash(this.state.total.total.toFixed(1))}</h1>}
+        {this.state.upgrades === false && resourcesArr.length> 5 && <h2 className='unlockUp' onClick={this.unlockUpgrades}    style={{background: `linear-gradient(180deg, rgba(255,255,266,1) ${(this.state.total.total/50000)*100}%, rgba(244,0,0,0.6292892156862745) 100%)`}}  >UNLOCK UPGRADES </h2>}
         <div className="columns parent">
 
 
@@ -127,13 +159,13 @@ class Main extends React.Component {
                 <div className ='column buy' onClick={this.buyBuilding} id={x.id}
                   style={{background: `linear-gradient(180deg, rgba(255,255,266,1) ${x.collected/(x.id*(x.amount*10))*100}%, rgba(244,0,0,0.6292892156862745) 100%)`}}
                 >
-
-
                   {x.amount}
                 </div>
                 {x.canCreate && !x.created && <div className='column'  id={x.id} onClick={this.newButton}>
                NEW
-
+                </div>}
+                {this.state.upgrades && <div className='column upgrade'  id={x.id} onClick={this.upgrade}   style={{background: `linear-gradient(180deg, rgba(255,255,266,1) ${x.collected/(x.id*(x.amount*10)*(x.upgradeLevel*10))*100}%, rgba(244,0,0,0.6292892156862745) 100%)`}}>
+                &#8682; {x.upgradeLevel.toFixed(1)}
                 </div>}
 
               </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -37,13 +37,13 @@ class Sidebar extends React.Component {
   }
 
 
-   formatCash(n) {
- if (n < 1e3) return n;
- if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
- if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
- if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
- if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-};
+  formatCash(n) {
+    if (n < 1e3) return n
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K'
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M'
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B'
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T'
+  }
 
   trade(e){
     if(resourcesArr.length>e.target.id){
